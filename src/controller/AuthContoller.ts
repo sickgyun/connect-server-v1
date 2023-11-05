baaafca920aed0ed6f6e1b58ee3564d5ac76e5a4
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-06 01:56:59 +0900

    refactor: error handling route 적용

--- src/controller/AuthContoller.ts
+++ src/controller/AuthContoller.ts
@@ -1,12 +1,32 @@
+import { BsmOauthError, BsmOauthErrorType } from 'bsm-oauth';
 import { Router } from 'express';
+import { BadRequestException, InternalServerException } from '../global/exception';
 import * as AuthService from '../service/AuthService';
 const router = Router();
 
 router.post('/', async (req, res, next) => {
   const { authCode } = req.body;
+  try {
+    const response = await AuthService.loginUser(authCode);
 
-  const response = await AuthService.loginUser(authCode, next);
-  return res.status(200).json(response);
+    return res.status(200).json(response);
+  } catch (error) {
+    if (error instanceof BsmOauthError) {
+      switch (error.type) {
+        case BsmOauthErrorType.INVALID_CLIENT: {
+          return next(new InternalServerException());
+        }
+        case BsmOauthErrorType.AUTH_CODE_NOT_FOUND: {
+          return next(new BadRequestException());
+        }
+        case BsmOauthErrorType.TOKEN_NOT_FOUND: {
+          return next(new InternalServerException());
+        }
+      }
+    } else {
+      return next(new InternalServerException());
+    }
+  }
 });
 
 export default router;
--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -13,53 +13,31 @@ import getEnvCofigs from '../global/env';
 const { BSM_AUTH_CLIENT_ID, BSM_AUTH_CLIENT_SECRET, JWT_SCRECT_KEY } = getEnvCofigs();
 const bsmOauth = new BsmOauth(BSM_AUTH_CLIENT_ID, BSM_AUTH_CLIENT_SECRET);
 
-export const loginUser = async (authCode: string, next: NextFunction) => {
-  try {
-    if (!authCode) return next(new BadRequestException());
-    const token = await bsmOauth.getToken(authCode as string);
-    const resource = await bsmOauth.getResource(token);
-    const { userCode, profileUrl } = resource;
-    if (resource) {
-      if (resource.role === BsmUserRole.STUDENT) {
-        const { name, grade } = resource.student;
-        let userRole: Role = Role.STUDENT;
-        // 졸업생이면
-        if (grade === 0) {
-          userRole = Role.GRADUATE;
-        }
-        const userInfo = {
-          id: userCode,
-          name: name,
-          profile_url: profileUrl,
-          role: userRole,
-          github_id: '',
-          semester: 0,
-        };
-        await UserRepository.upsertUser(userInfo);
-      } else {
-        return next(new ForbiddenException());
-      }
-      const jwtToken = jwt.sign({ userCode }, JWT_SCRECT_KEY, { expiresIn: '30m' });
-      return {
-        message: '로그인 성공',
-        data: { accessToken: jwtToken, refreshToken: jwtToken },
-      };
+export const loginUser = async (authCode: string) => {
+  const token = await bsmOauth.getToken(authCode as string);
+  const resource = await bsmOauth.getResource(token);
+  const { userCode, profileUrl } = resource;
+  if (resource.role === BsmUserRole.STUDENT) {
+    const { name, grade } = resource.student;
+    let userRole: Role = Role.STUDENT;
+    // 졸업생이면
+    if (grade === 0) {
+      userRole = Role.GRADUATE;
     }
-  } catch (error) {
-    if (error instanceof BsmOauthError) {
-      switch (error.type) {
-        case BsmOauthErrorType.INVALID_CLIENT: {
-          return next(new InternalServerException());
-        }
-        case BsmOauthErrorType.AUTH_CODE_NOT_FOUND: {
-          return next(new BadRequestException());
-        }
-        case BsmOauthErrorType.TOKEN_NOT_FOUND: {
-          return next(new InternalServerException());
-        }
-      }
-    } else {
-      return next(new InternalServerException());
-    }
+    const userInfo = {
+      id: userCode,
+      name: name,
+      profile_url: profileUrl,
+      role: userRole,
+      github_id: '',
+      semester: 0,
+    };
+    await UserRepository.upsertUser(userInfo);
+
+    const jwtToken = jwt.sign({ userCode }, JWT_SCRECT_KEY, { expiresIn: '30m' });
+    return {
+      message: '로그인 성공',
+      data: { accessToken: jwtToken, refreshToken: jwtToken },
+    };
   }
 };
