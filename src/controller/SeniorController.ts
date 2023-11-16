9348297b6df60e8fa6fc05730d892b075f1bf8eb
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-16 21:20:22 +0900

    feat: 프로필 등록시 졸업생이 아닐때 에러 발생 제거

--- src/controller/SeniorController.ts
+++ src/controller/SeniorController.ts
@@ -24,11 +24,6 @@ router.post('/', async (req: Request, res: Response) => {
     return;
   }
 
-  if (!isGraduate) {
-    generateError({ message: '졸업생이 아닙니다', status: 401 });
-    return;
-  }
-
   const token = authorization.split('Bearer ')[1];
   const decodedJwt = jwtDecode<{ userCode: number }>(token);
 
--- src/service/SeniorService.ts
+++ src/service/SeniorService.ts
@@ -6,13 +6,13 @@ export const createSenior = async (seniorInformation: Senior) => {
   const isValidSenior = await SeniotRepository.findUnique(seniorInformation.userCode);
 
   if (isValidSenior) {
-    generateError({ message: '이미 존재하는 Senior입니다.', status: 409 });
+    generateError({ message: '이미 존재하는 학생입니다.', status: 409 });
     return;
   }
 
   await SeniotRepository.create(seniorInformation);
 
-  return { message: '선배 프로필 작성 성공' };
+  return { message: '프로필 작성 성공' };
 };
 
 export const getSenior = async (userCode: number) => {
