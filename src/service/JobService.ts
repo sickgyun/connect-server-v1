aa86764da736f3425e8648e421887c52808e887e
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-19 14:41:19 +0900

    chore: 채용공고 4개만 슬라이스

--- src/service/JobService.ts
+++ src/service/JobService.ts
@@ -16,12 +16,14 @@ export const getJobList = async () => {
   let jobList: Job[] = [];
 
   $('.css-mao678 > li').each((index, item) => {
-    const title = $(item).find('.summary__title').text();
-    const image = $(item).find('img').attr('src');
-    const company = $(item).find('.summary__company-name').text();
-    const link = 'https://www.rallit.com/' + $(item).find('a').attr('href');
+    if (index < 4) {
+      const title = $(item).find('.summary__title').text();
+      const image = $(item).find('img').attr('src');
+      const company = $(item).find('.summary__company-name').text();
+      const link = 'https://www.rallit.com/' + $(item).find('a').attr('href');
 
-    jobList.push({ title, image, company, link });
+      jobList.push({ title, image, company, link });
+    }
   });
 
   return { message: '성공', dataList: jobList };
