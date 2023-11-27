ec648dd44d68e853cb0e62908c7dd1f3ad51cad3
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-27 10:23:26 +0900

    feat: 협약 회사 수정, 생성 어드민 처리 추가

--- src/controller/MouCompanyController.ts
+++ src/controller/MouCompanyController.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from 'express';
 import * as MouCompanyService from '../service/MouCompanyService';
+import { jwtDecode } from 'jwt-decode';
+import { generateError } from '../middleware/errorHandler';
+import { Role } from '@prisma/client';
+import { BsmUserRole } from 'bsm-oauth';
 
 const router = Router();
 
@@ -10,7 +14,21 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 router.post('/', async (req: Request, res: Response) => {
+  const { authorization } = req.headers;
   const { companyName, detailUrl, profileUrl, category, major } = req.body;
+
+  if (!authorization) {
+    generateError({ message: '토큰이 비어 있습니다', status: 403 });
+    return;
+  }
+
+  const token = authorization.split('Bearer ')[1];
+  const decodedJwt = jwtDecode<{ role: Role }>(token);
+
+  if (decodedJwt.role !== BsmUserRole.TEACHER) {
+    generateError({ message: '잘못된 접근입니다.', status: 403 });
+    return;
+  }
 
   const mouCompany = {
     companyName: companyName,
@@ -26,8 +44,22 @@ router.post('/', async (req: Request, res: Response) => {
 });
 
 router.patch('/:mouCompanyId', async (req: Request, res: Response) => {
+  const { authorization } = req.headers;
   const { mouCompanyId } = req.params;
   const { companyName, detailUrl, profileUrl, category, major } = req.body;
+
+  if (!authorization) {
+    generateError({ message: '토큰이 비어 있습니다', status: 403 });
+    return;
+  }
+
+  const token = authorization.split('Bearer ')[1];
+  const decodedJwt = jwtDecode<{ role: Role }>(token);
+
+  if (decodedJwt.role !== BsmUserRole.TEACHER) {
+    generateError({ message: '잘못된 접근입니다.', status: 403 });
+    return;
+  }
 
   const mouCompany = {
     id: Number(mouCompanyId),
